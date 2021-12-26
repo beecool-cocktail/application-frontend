4585a2288d77b91cfb8d009e4fc7d4258d060988
Raven Pan <raven.pan@framy.co>
2021-12-26 15:26:54 +0800

    feat: add api rewrites for stage env

--- next.config.js
+++ next.config.js
@@ -4,12 +4,34 @@ module.exports = {
   async rewrites() {
     return [
       {
+        has: [{ type: 'host', value: 'localhost' }],
         source: '/api/:path*',
         destination: 'http://localhost:6969/api/:path*'
       },
       {
+        has: [{ type: 'host', value: 'localhost' }],
         source: '/static/:path*',
         destination: 'http://localhost:6969/static/:path*'
+      },
+      {
+        has: [
+          {
+            type: 'host',
+            value: 'application-frontend-mhkzpmkvca-de.a.run.app'
+          }
+        ],
+        source: '/api/:path*',
+        destination: 'http://whisperingcorner.zapto.org/api/:path*'
+      },
+      {
+        has: [
+          {
+            type: 'host',
+            value: 'application-frontend-mhkzpmkvca-de.a.run.app'
+          }
+        ],
+        source: '/static/:path*',
+        destination: 'http://whisperingcorner.zapto.org/static/:path*'
       }
     ]
   }
