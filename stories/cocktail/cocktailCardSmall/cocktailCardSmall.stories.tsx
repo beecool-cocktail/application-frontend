6f82e7818245a2e3d1931a761c327dae6cc5e0bb
Raven Pan <galtz0321@gmail.com>
2022-07-21 16:10:25 +0800

    feat: cocktail card story with figma design link

--- stories/cocktail/cocktailCardSmall/cocktailCardSmall.stories.tsx
+++ stories/cocktail/cocktailCardSmall/cocktailCardSmall.stories.tsx
@@ -16,7 +16,7 @@ export default {
   parameters: {
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/file/9BFjANqSdCCk0cV8obeMCs/Whispering-Corner-Mobile?node-id=2560%3A5124'
+      url: 'https://www.figma.com/file/9BFjANqSdCCk0cV8obeMCs/Whispering-Corner-Mobile?node-id=4613%3A11271'
     }
   },
   decorators: [
