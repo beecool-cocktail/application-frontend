3e6c59187503006e045c9fd080e2e8729fdf2a2c
Raven Pan <galtz0321@gmail.com>
2022-07-21 23:02:41 +0800

    fix: favorite cocktail list does not show cards

--- components/common/favoriteCocktailList/favoriteCocktailList.tsx
+++ components/common/favoriteCocktailList/favoriteCocktailList.tsx
@@ -20,6 +20,7 @@ const FavoriteCocktailCardList = ({
     gotoCocktailPage,
     removeCocktail
   } = useFavoriteCocktailList(userId)
+
   if (error) return <Error />
   if (!list || loading) return <Loading />
   if (!list.isPublic && userId)
@@ -28,9 +29,15 @@ const FavoriteCocktailCardList = ({
     return <Typography variant="h4">沒有收藏 QQ</Typography>
 
   return (
-    <Grid container alignItems="flex-start" rowSpacing={1} columnSpacing={1}>
+    <Grid
+      container
+      alignItems="flex-start"
+      rowSpacing="8px"
+      columnSpacing="8px"
+      sx={{ p: '8px', color: theme => theme.palette.dark3.main }}
+    >
       {list.data.map(cocktail => (
-        <Grid item xs={6} key={cocktail.id}>
+        <Grid item xs={6} key={cocktail.id} sx={{ aspectRatio: '176/171' }}>
           <CocktailCardSmall
             cocktail={cocktail}
             actions={[
