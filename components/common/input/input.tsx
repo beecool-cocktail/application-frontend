313ff87fad0ec837cbe471f6140fd2e94681b633
Raven Pan <galtz0321@gmail.com>
2022-07-30 16:47:31 +0800

    fix: input style not follow design guideline

--- components/common/input/input.tsx
+++ components/common/input/input.tsx
@@ -19,12 +19,16 @@ interface optionalControlProps {
   defaultValue?: InputBaseProps['defaultValue']
   valueFromProps?: InputBaseProps['value']
   onChangeFromProps: InputBaseProps['onChange']
+  getLetterCount: (target: string) => number
+  maxLength?: number
 }
 
 const useOptionalControl = ({
   defaultValue,
   valueFromProps,
-  onChangeFromProps
+  onChangeFromProps,
+  getLetterCount,
+  maxLength
 }: optionalControlProps) => {
   const { current: isControlled } = useRef(valueFromProps != null)
   const { current: hasDefaultValue } = useRef(defaultValue != null)
@@ -32,16 +36,20 @@ const useOptionalControl = ({
     hasDefaultValue ? defaultValue : ''
   )
   const value = String(isControlled ? valueFromProps : internalValue)
+  const letterCount = getLetterCount(value)
 
   const handleChange = (
     e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
   ) => {
+    const letterCount = getLetterCount(e.target.value)
+    if (maxLength && letterCount > maxLength) return
     if (onChangeFromProps) onChangeFromProps(e)
     if (!isControlled) setInternalValue(e.target.value)
   }
 
   return {
     value,
+    letterCount,
     isControlled,
     hasDefaultValue,
     handleChange
@@ -58,13 +66,16 @@ const RawInput = (props: InputProps) => {
     placeholder,
     maxLength,
     multiline,
-    getLetterCount = target => target.length
+    getLetterCount = target => target.length,
+    ...restProps
   } = props
   const formControl = useFormControl()
-  const { value, handleChange } = useOptionalControl({
+  const { value, letterCount, handleChange } = useOptionalControl({
     defaultValue,
     valueFromProps,
-    onChangeFromProps
+    onChangeFromProps,
+    getLetterCount,
+    maxLength
   })
 
   const getPlaceholder = () => {
@@ -77,7 +88,17 @@ const RawInput = (props: InputProps) => {
     e.target.value.replace(/  +/g, ' ')
   }
 
-  const letterCount = getLetterCount(value)
+  const renderLetterCount = () => (
+    <Typography
+      variant="body4"
+      sx={{
+        ml: 'auto',
+        color: theme => theme.palette.light4.main
+      }}
+    >
+      {`${letterCount}/${maxLength}`}
+    </Typography>
+  )
 
   return (
     <Stack alignItems="flex-start" spacing="4px" sx={{ position: 'relative' }}>
@@ -89,90 +110,97 @@ const RawInput = (props: InputProps) => {
           {label}
         </Typography>
       )}
-      <InputBase
-        {...props}
+      <Stack
         sx={{
-          display: 'flex',
-          alignItems: 'flex-start',
+          justifyContent: 'space-between',
+          gap: '4px',
+          width: 1,
           minHeight: multiline ? '186px' : '50px',
-          p: () => {
-            if (!multiline) return '10px 11px'
-            return '16px 20px'
+          bgcolor: theme => {
+            if (formControl && (formControl.focused || formControl.error))
+              return theme.palette.dark6.main
+            return theme.palette.dark5.main
           },
-          columnGap: '4px',
           borderRadius: '8px',
           border: theme =>
             `2px solid ${
               formControl?.error ? theme.palette.red.main : 'transparent'
             }`,
-          color: theme => {
-            if (formControl && (formControl.focused || formControl.error))
-              return theme.palette.light1.main
-            return theme.palette.light2.main
-          },
-          backgroundColor: theme => {
-            if (formControl && (formControl.focused || formControl.error))
-              return theme.palette.dark6.main
-            return theme.palette.dark5.main
-          },
-          fontSize: theme => theme.typography.body1.fontSize,
-          fontWeight: theme => theme.typography.body1.fontWeight,
-          lineHeight: theme => theme.typography.body1.lineHeight,
-          '&::placeholder': {
-            color: theme => theme.palette.light4.main
-          },
-          '& svg': {
-            p: 0,
-            fontSize: '24px'
-          },
-          '& svg *': {
-            stroke: theme => {
-              const { light1, light2, light4 } = theme.palette
-              if (formControl && formControl.focused) return light1.main
-              if (formControl && formControl.filled) return light2.main
-              return light4.main
-            }
-          },
-          '& .MuiInputBase-input': {
-            p: 0,
-            display: 'flex',
-            alignItems: 'center'
-          },
-          ...props.sx
+          p: () => {
+            if (!multiline) return '11px 10px'
+            return '6px 10px'
+          }
         }}
-        inputProps={{ ...props.inputProps, maxLength }}
-        placeholder={getPlaceholder()}
-        onChange={handleChange}
-        onBlur={handleBlur}
-      />
-      {feedback && (
-        <Typography
-          variant="body2"
-          sx={{ pl: '6px', color: theme => theme.palette.light4.main }}
-        >
-          {feedback}
-        </Typography>
-      )}
-      {maxLength && (
-        <Typography
-          variant="body4"
+      >
+        <InputBase
+          {...restProps}
           sx={{
-            pl: '6px',
-            color: theme => theme.palette.light4.main,
-            position: () => {
-              if (multiline) return 'absolute'
+            display: 'flex',
+            alignItems: 'flex-start',
+            minHeight: '0',
+            columnGap: '4px',
+            p: 0,
+            lineHeight: 0,
+            color: theme => {
+              if (formControl && (formControl.focused || formControl.error))
+                return theme.palette.light1.main
+              return theme.palette.light2.main
             },
-            right: () => {
-              if (multiline) return '12px'
+
+            '&::placeholder': {
+              color: theme => theme.palette.light4.main
             },
-            bottom: () => {
-              if (multiline) return '8px'
-            }
+            '& svg': {
+              p: 0,
+              fontSize: '24px'
+            },
+            '& svg *': {
+              stroke: theme => {
+                const { light1, light2, light4 } = theme.palette
+                if (formControl && formControl.focused) return light1.main
+                if (formControl && formControl.filled) return light2.main
+                return light4.main
+              }
+            },
+            '& .MuiInputBase-input': {
+              flex: 1,
+              width: 'auto',
+              height: 'auto',
+              p: 0,
+              pt: multiline ? '8px' : 0,
+              fontSize: theme => theme.typography.body1.fontSize,
+              fontWeight: theme => theme.typography.body1.fontWeight,
+              lineHeight: theme => theme.typography.body1.lineHeight
+            },
+            ...restProps.sx
           }}
-        >
-          {`${letterCount}/${maxLength}`}
-        </Typography>
-      )}
+          value={value}
+          inputProps={props.inputProps}
+          multiline={multiline}
+          placeholder={getPlaceholder()}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {maxLength && multiline && renderLetterCount()}
+      </Stack>
+      <Stack
+        direction="row"
+        alignItems="flex-start"
+        justifyContent="space-between"
+        width={1}
+        pl="6px"
+        pr="12px"
+      >
+        {feedback && (
+          <Typography
+            variant="body2"
+            sx={{ color: theme => theme.palette.light4.main }}
+          >
+            {feedback}
+          </Typography>
+        )}
+        {maxLength && !multiline && renderLetterCount()}
+      </Stack>
     </Stack>
   )
 }
