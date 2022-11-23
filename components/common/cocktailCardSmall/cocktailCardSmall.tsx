7d806117887e7cada749676ec0b5d4f97d0a9bec
Raven Pan <raven.pan@frontier.cool>
2022-11-23 20:12:19 +0800

    fix(cocktailCardSmall): click menu action item not work

--- components/common/cocktailCardSmall/cocktailCardSmall.tsx
+++ components/common/cocktailCardSmall/cocktailCardSmall.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Image from 'next/image'
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Stack, Typography, ClickAwayListener } from '@mui/material'
 import { ProfileCocktailItem } from 'lib/domain/cocktail'
 import MoreIcon from 'lib/assets/more.svg'
 import CloseIcon from 'lib/assets/cancelClose.svg'
@@ -26,115 +26,121 @@ const CocktailCardSmall = ({
   defaultEditMode = false
 }: FavoriteCocktailCardProps) => {
   const [isEditMode, setEditMode] = useState(defaultEditMode)
+  const preventOpenCocktailRef = useRef(false)
 
-  const handleClose = (e: React.SyntheticEvent) => {
-    e.preventDefault()
+  const handleClose = () => {
     setEditMode(false)
   }
 
-  const handleClickMoreAction = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
-    e.stopPropagation()
+  const handleClickMoreAction = () => {
     setEditMode(editMode => !editMode)
+    preventOpenCocktailRef.current = true
+  }
+
+  const handleCardClick = () => {
+    if (preventOpenCocktailRef.current) return
+    onClick(cocktail.id)
+    preventOpenCocktailRef.current = false
   }
 
   const buttonActions = actions.map(action => ({
     ...action,
-    onClick: (e: React.MouseEvent) => {
-      handleClose(e)
+    onClick: () => {
+      handleClose()
       action.onClick(cocktail)
     }
   }))
 
   return (
-    <Box
-      sx={{
-        position: 'relative',
-        width: 1,
-        height: 1,
-        borderRadius: '6px',
-        cursor: isEditMode ? 'normal' : 'pointer',
-        overflow: 'hidden'
-      }}
-      onClick={() => onClick(cocktail.id)}
-      onBlur={handleClose}
-    >
-      <Image
-        style={{ borderRadius: '6px' }}
-        layout="fill"
-        objectFit="cover"
-        src={cocktail.photoUrl}
-        alt="favorite cocktail image"
-        width={400}
-        height={300}
-      />
-      {isEditMode && (
-        <Stack
-          sx={{
-            position: 'absolute',
-            alignItems: 'center',
-            top: 0,
-            left: 0,
-            width: 1,
-            height: 1,
-            background: 'rgba(0, 0, 0, 0.5)',
-            '& > *': {
-              mt: '20px'
-            }
-          }}
-        >
-          {buttonActions.map(({ text, onClick }) => (
-            <ActionButton key={text} text={text} onClick={onClick} />
-          ))}
-        </Stack>
-      )}
+    <ClickAwayListener onClickAway={handleClose}>
       <Box
         sx={{
-          position: 'absolute',
-          display: 'flex',
-          alignItems: 'flex-end',
+          position: 'relative',
           width: 1,
-          height: '45px',
-          bottom: 0,
-          left: 0,
+          height: 1,
           borderRadius: '6px',
-          background:
-            'radial-gradient(198.57% 198.57% at 50% 207.14%, rgba(0, 0, 0, 0.6) 60.51%, rgba(0, 0, 0, 0) 100%)'
+          cursor: isEditMode ? 'normal' : 'pointer',
+          overflow: 'hidden'
         }}
+        onClick={handleCardClick}
       >
-        <Stack
-          direction="row"
-          sx={{
-            width: 1,
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            p: '4px',
-            pr: '8px'
-          }}
-        >
-          <Typography
-            variant="body3"
+        <Image
+          style={{ borderRadius: '6px' }}
+          layout="fill"
+          objectFit="cover"
+          src={cocktail.photoUrl}
+          alt="favorite cocktail image"
+          width={400}
+          height={300}
+        />
+        {isEditMode && (
+          <Stack
             sx={{
-              width: 1,
-              display: 'flex',
+              position: 'absolute',
               alignItems: 'center',
-              color: theme => theme.palette.brandWhite.main,
-              background:
-                'linear-gradient(90deg, #FFFFFF 42.53%, rgba(255, 255, 255, 0) 80%)',
-              backgroundClip: 'text',
-              overflow: 'hidden',
-              whiteSpace: 'nowrap',
-              textFillColor: 'transparent'
+              top: 0,
+              left: 0,
+              width: 1,
+              height: 1,
+              background: 'rgba(0, 0, 0, 0.5)',
+              '& > *': {
+                mt: '20px'
+              }
             }}
           >
-            {cocktail.title}
-          </Typography>
-          <IconButton size={18} onClick={handleClickMoreAction}>
-            {isEditMode ? <CloseIcon /> : <MoreIcon />}
-          </IconButton>
-        </Stack>
+            {buttonActions.map(({ text, onClick }) => (
+              <ActionButton key={text} text={text} onClick={onClick} />
+            ))}
+          </Stack>
+        )}
+        <Box
+          sx={{
+            position: 'absolute',
+            display: 'flex',
+            alignItems: 'flex-end',
+            width: 1,
+            height: '45px',
+            bottom: 0,
+            left: 0,
+            borderRadius: '6px',
+            background:
+              'radial-gradient(198.57% 198.57% at 50% 207.14%, rgba(0, 0, 0, 0.6) 60.51%, rgba(0, 0, 0, 0) 100%)'
+          }}
+        >
+          <Stack
+            direction="row"
+            sx={{
+              width: 1,
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              p: '4px',
+              pr: '8px'
+            }}
+          >
+            <Typography
+              variant="body3"
+              sx={{
+                width: 1,
+                display: 'flex',
+                alignItems: 'center',
+                color: theme => theme.palette.brandWhite.main,
+                background:
+                  'linear-gradient(90deg, #FFFFFF 42.53%, rgba(255, 255, 255, 0) 80%)',
+                backgroundClip: 'text',
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                textFillColor: 'transparent'
+              }}
+            >
+              {cocktail.title}
+            </Typography>
+            <IconButton size={18} onClick={handleClickMoreAction}>
+              {isEditMode ? <CloseIcon /> : <MoreIcon />}
+            </IconButton>
+          </Stack>
+        </Box>
       </Box>
-    </Box>
+    </ClickAwayListener>
   )
 }
 
