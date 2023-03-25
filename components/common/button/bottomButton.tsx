c0c53d7b0b7f0088174872cd87bf1322fcb70855
Raven Pan <raven.pan@frontier.cool>
2023-03-26 00:05:35 +0800

    feat(bottomButton): set maximum width

--- components/common/button/bottomButton.tsx
+++ components/common/button/bottomButton.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material'
 import Button, { ButtonProps } from 'components/common/button/button'
+import { MAX_WIDTH } from 'lib/constants/layout'
 
 interface BottomButton extends ButtonProps {
   position?: 'static' | 'fixed'
@@ -10,12 +11,15 @@ const BottomButton = (props: BottomButton) => {
     <Box
       sx={{
         position: props.position,
+        margin: '0 auto',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         bottom: 0,
         left: 0,
+        right: 0,
         width: 1,
+        maxWidth: MAX_WIDTH,
         height: 124,
         px: '40px',
         py: '40px',
