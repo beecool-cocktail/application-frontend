6f38f2f61be5636961d421b82567f34ae9932afe
Raven Pan <raven.pan@frontier.cool>
2023-04-22 18:00:19 +0800

    feat: logo header apply spring animation

--- components/layout/logoHeader.tsx
+++ components/layout/logoHeader.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { useSpring, animated } from 'react-spring'
 import { Box, Stack } from '@mui/material'
 import throttle from 'lodash.throttle'
 import LogoSVG from 'lib/assets/logo.svg'
@@ -7,8 +8,21 @@ import BottomLineSVG from 'lib/assets/bottomLine.svg'
 
 const Logo = () => {
   const [useSmallLog, setUseSmallLogo] = useState(false)
+  const prevUseSmallLogRef = useRef(useSmallLog)
+  const [springs, api] = useSpring(() => {
+    return {
+      y: 0,
+      config: {
+        tension: 300,
+        friction: 20,
+        mass: 1
+      }
+    }
+  })
 
   useEffect(() => {
+    setUseSmallLogo(window.scrollY > 0)
+
     const scrollHandler = throttle(() => {
       if (window.scrollY > 0) setUseSmallLogo(true)
       else setUseSmallLogo(false)
@@ -20,6 +34,17 @@ const Logo = () => {
       window.removeEventListener('scroll', scrollHandler)
     }
   }, [])
+
+  useEffect(() => {
+    if (useSmallLog !== prevUseSmallLogRef.current) {
+      api.start({
+        from: { y: 0 },
+        to: { y: 52 },
+        reverse: !useSmallLog
+      })
+    }
+    prevUseSmallLogRef.current = useSmallLog
+  }, [api, useSmallLog])
 
   return (
     <>
@@ -33,27 +58,42 @@ const Logo = () => {
       >
         <LogoSVG width="100%" height={77} />
       </Box>
-      <Box
-        position="fixed"
-        width="100%"
-        sx={{
+      <animated.div
+        style={{
+          position: 'fixed',
+          width: '100%',
           top: -52,
           height: 52,
-          bgcolor: theme => theme.palette.background.default,
-          transitionDuration: '0.1s',
-          transform: useSmallLog ? 'translateY(52px)' : '',
-          zIndex: 1300
+          zIndex: 1300,
+          ...springs
         }}
       >
-        <Stack direction="row" columnGap="8px">
-          <Box pt="10px" lineHeight={1}>
-            <BottomLineSVG width={25} height={25} />
-          </Box>
-          <Box pt="16px" lineHeight={1}>
-            <CornerSVG width={111} height={16} />
-          </Box>
-        </Stack>
-      </Box>
+        <Box
+          position="absolute"
+          sx={{
+            width: 1,
+            height: 20,
+            top: -20,
+            bgcolor: theme => theme.palette.background.default
+          }}
+        ></Box>
+        <Box
+          sx={{
+            width: 1,
+            height: 1,
+            bgcolor: theme => theme.palette.background.default
+          }}
+        >
+          <Stack direction="row" columnGap="8px">
+            <Box pt="10px" lineHeight={1}>
+              <BottomLineSVG width={25} height={25} />
+            </Box>
+            <Box pt="16px" lineHeight={1}>
+              <CornerSVG width={111} height={16} />
+            </Box>
+          </Stack>
+        </Box>
+      </animated.div>
     </>
   )
 }
