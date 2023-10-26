749ee219b6c7f5c02799ad888cdf51c00c506e7a
Raven Pan <raven.pan@frontier.cool>
2023-10-26 20:48:28 +0800

    feat: website favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,6 @@ const Home = () => {
       <Head>
         <title>{websiteTitle}</title>
         <meta name="description" content={websiteTitle} />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <CocktailList />
     </>
